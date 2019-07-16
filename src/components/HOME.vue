ead94c23f3759c2d47e5f3d0bcebd6df50847f90
1512190320 <452954869@qq.com>
2019-07-16 16:49:01 +0800

    add login

--- src/components/HOME.vue
+++ src/components/HOME.vue
@@ -1,45 +1,69 @@
 <template>
-<my-container >
-  <div id="app">
-    <div class="header"><Header/></div>
-    <div class="background"><Background/></div>
-    <!-- <div class="moneycard"><MoneyCard/></div>
-    <div class="echarts"><Echarts/></div>
-    <div class="piechart"><PieChart/></div>
-    <div class="buttonPanel"><ButtonPanel/></div> -->
-  </div>
-</my-container>
+  <my-container>
+    <div id="app">
+      <!-- <test  @getLogin="getLogin"></test> -->
+      <div v-if="login">
+        <div class="header">
+          <Header />
+        </div>
+        <div class="background">
+          <Background />
+        </div>
+        <!-- <div class="moneycard"><MoneyCard/></div>
+      <div class="echarts"><Echarts/></div>
+      <div class="piechart"><PieChart/></div>
+        <div class="buttonPanel"><ButtonPanel/></div>-->
+      </div>
+      <div v-else>
+        <div class="login">
+          <Login @getLogin="getLogin"/>
+        </div>
+      </div>
+    </div>
+  </my-container>
 </template>
 
 <script>
-import Header from './Header'
-import Background from './Background'
+import Header from "./Header";
+import Background from "./Background";
+import Login from "./Login";
 // import MoneyCard from './MoneyCard'
 // import Echarts from './ECharts'
 // import PieChart from './PieChart'
 // import ButtonPanel from './ButtonPanel'
 export default {
-  name: 'App',
-  components:{
+  name: "App",
+  components: {
     Header,
     Background,
+    Login
     // MoneyCard,
     // Echarts,
     // PieChart,
     // ButtonPanel
+  },
+  data() {
+    return {
+      login: false
+    };
+  },
+  methods:{
+    getLogin(msg){//msg就是传过来的数据了  这只是个形参  名字可以随意
+      this.login=msg;//然后将数据赋值给chindVal
+    }
   }
-}
+};
 </script>
 
 <style scoped>
-.el-row{
+.el-row {
   background-color: rgba(143, 0, 0, 0.288);
 }
-body{
-  margin:0px;
+body {
+  margin: 0px;
 }
-:root{
-  font-size:10px;
+:root {
+  font-size: 10px;
 }
 #app {
   font-family: "Open Sans", Arial, sans-serif;
@@ -48,7 +72,7 @@ body{
   text-align: center;
   color: #2c3e50;
 }
-.header{
+.header {
   top: 0;
   left: 0;
   margin: 0;
@@ -56,12 +80,10 @@ body{
   width: 100%;
   z-index: 3000;
 }
-.background{
+.background {
   position: absolute;
   top: 0;
   left: 0;
-  z-index:0;
+  z-index: 0;
 }
-
-
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="login-container">
+    <el-form
+      :model="ruleForm2"
+      :rules="rules2"
+      ref="ruleForm2"
+      label-position="left"
+      label-width="0px"
+      class="login-page"
+    >
+      <h3 class="title">系统登录</h3>
+      <el-form-item prop="userId">
+        <el-input type="text" v-model="ruleForm2.userId" auto-complete="off" placeholder="用户名"></el-input>
+      </el-form-item>
+      <el-form-item prop="userPsw">
+        <el-input
+          type="password"
+          v-model="ruleForm2.userPsw"
+          auto-complete="off"
+          placeholder="密码"
+          show-password
+        ></el-input>
+      </el-form-item>
+      <el-checkbox v-model="checked" class="rememberme">记住密码</el-checkbox>
+      <el-form-item style="width:100%;">
+        <el-button type="primary" style="width:100%;" @click="handleSubmit" :loading="login">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      login: false,
+      ruleForm2: {
+        userId: "",
+        userPsw: ""
+      },
+      rules2: {
+        userId: [
+          {
+            required: true,
+            message: "please enter your account",
+            trigger: "blur"
+          }
+        ],
+        userPsw: [
+          { required: true, message: "enter your userPsw", trigger: "blur" }
+        ]
+      },
+      checked: false
+    };
+  },
+  methods: {
+    handleSubmit(event) {
+      var _self = this;
+      this.$axios
+        .post("http://localhost:6060/user/signIn/", this.ruleForm2)
+        .then(response => {
+          var data = response.data;
+
+          console.log(data);
+          
+          this.login = true
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+   watch:{
+    'login':function(){
+      this.$emit('getLogin',this.login);//主要是通过$emit方法来实现传参的方式，第一个参数是自定义事件名称，第二个则是要传的数据
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login-container {
+  width: 100%;
+  height: 100%;
+}
+.login-page {
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+  margin: 180px auto;
+  width: 350px;
+  padding: 35px 35px 15px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+}
+label.el-checkbox.rememberme {
+  margin: 0px 0px 15px;
+  text-align: left;
+}
+</style>
