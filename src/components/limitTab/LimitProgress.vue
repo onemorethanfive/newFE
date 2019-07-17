e38d3a1034492575fe88c9fc94d5693dd126f0ea
wen <123@qq.com>
2019-07-18 03:49:38 +0800

    limitTab

--- src/components/limitTab/LimitProgress.vue
+++ src/components/limitTab/LimitProgress.vue
@@ -1,25 +1,40 @@
 <template>
   <div class="limitProgress">
     <el-row type="flex" justify="center">
-      <el-col :span="4">
-        <div class="moneytt" style="text-align:">日期进度:</div>
+      <el-col :span="6">
+        <el-row class="moneytt" style="text-align:">开始日期:</el-row>
+        <el-row class="moneytt" style="text-align:">{{startDate}}</el-row>
       </el-col>
     </el-row>
-    <el-row>
-      <el-col :span="5" align="center">
-        <div class="moneytt"></div>
-      </el-col>
-    </el-row>
-    <el-progress :text-inside="true" :stroke-width="50" :percentage="50" status="exception"></el-progress>
+    <el-progress
+      :text-inside="true"
+      :stroke-width="50"
+      :percentage="dateProcess"
+      :status="getProcessStatus(dateProcess)"
+    ></el-progress>
 
     <el-row type="flex" justify="center">
-      <el-col :span="4">
-        <div class="moneytt" style="text-align:">总限额:</div>
-        <el-progress type="circle" :percentage="0" :width="150" :show-text="true"></el-progress>
+      <el-col :xs="{span:10,offset:0,font:6}" :sm="{span:8,offset:4}" :md="{span:6,offset:6}">
+        <div class="moneytt">总限额:</div>
+        <div class="moneytt">{{restTotal}}/{{limitTotal}}</div>
+        <el-progress
+          type="circle"
+          :percentage="totalProcess"
+          :width="150"
+          :show-text="true"
+          :status="getProcessStatus(totalProcess)"
+        ></el-progress>
       </el-col>
-        <el-col :span="4">
-        <div class="moneytt" style="text-align:">今日限额:</div>
-         <el-progress type="circle" :percentage="100" :width="150" :show-text="true" status="exception"></el-progress>
+      <el-col :xs="{span:10,offset:0}" :sm="{span:8,offset:4}" :md="{span:6,offset:6}">
+        <div class="moneytt">今日限额:</div>
+        <div class="moneytt">{{restToday}}/{{limitToday}}</div>
+        <el-progress
+          type="circle"
+          :percentage="todayProcess"
+          :width="150"
+          :show-text="true"
+          :status="getProcessStatus(todayProcess)"
+        ></el-progress>
       </el-col>
     </el-row>
     <el-row>
@@ -27,8 +42,6 @@
         <div class="moneytt"></div>
       </el-col>
     </el-row>
-    
-   
   </div>
 </template>
 
@@ -37,28 +50,56 @@ export default {
   name: "LimitProgress",
   data() {
     return {
-        userId:1,
-        dateProcess:"",
-        totalProcess: "",
-        todayProcess: ""
-
+      userId: 1,
+      dateProcess: 0,
+      totalProcess: 0,
+      todayProcess: 0,
+      today: "20190718",
+      limitToday: "",
+      limitTotal: "",
+      pastDays: "",
+      restToday: "",
+      restTotal: "",
+      startDate: "",
+      totalDay: ""
     };
   },
   methods: {
+    getProcessStatus(percentage) {
+      console.log("percentage"+percentage)
+      if (percentage < 25) {
+        return "success";
+      } else if (percentage < 50) {
+        return "";
+      } else if (percentage < 75) {
+        return "warning";
+      } else {
+        return "exception";
+      }
+    },
     getData() {
       var _self = this;
       this.$axios
         .get(_self.$root.urlport+"/userSpend/updateUserSpend", {
           params: {
-            userId: this.userId
+            userId: this.userId,
+            today: this.today
           }
         })
         .then(response => {
           var bills = response.data;
           var data = response.data;
           _self.billData = data;
+          _self.limitToday = data.limitToday.slice(0, 5);
+          _self.limitTotal = data.limitTotal;
+          _self.pastDays = data.pastDays;
+          _self.restToday = data.restToday.slice(0, 5);
+          _self.restTotal = data.restTotal;
+          _self.startDate = data.startDate;
+          _self.dateProcess = Math.round(data.pastDays/data.totalDay*100);
+          _self.totalProcess = 100-Math.round(data.restTotal / data.limitTotal * 100);
+          _self.todayProcess = 100-Math.round(data.restToday / data.limitToday * 100);
           console.log(data);
-          // console.log(bills[0]);
         })
         .catch(error => {
           console.log(error);
@@ -67,7 +108,6 @@ export default {
   },
   mounted() {
     this.getData();
-    
   },
   created: function() {}
 };
@@ -78,5 +118,14 @@ export default {
 .box-card1 {
   width: 50%;
   margin: 0 auto;
+}
+.moneytt {
+  text-align: center;
+}
+.el-row {
+  background-color: transparent;
+}
+.div {
+  font-size: 1rem;
 }
 </style>
